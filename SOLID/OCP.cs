d8175eab1db709bda76badd90598ed41e53093f8
MagicEaster <101589700+MagicEaster@users.noreply.github.com>
2023-03-04 10:04:01 +0300

    OCP done

---
+++ SOLID/OCP.cs
@@ -0,0 +1,58 @@
+﻿namespace SOLID
+{
+    //плохой пример открытости\закрытости
+    public class EmployeeReport
+    {
+        // <summary>
+        /// Тип отчета
+        /// </summary>
+        public string TypeReport { get; set; }
+
+        /// <summary>
+        /// товарная-накладная
+        /// </summary>
+        public void GenerateConsignmentNote(Storage storage)
+        {
+                    
+            if (TypeReport == "CSV")
+            {
+                // Генерация товарной-накладной в формате CSV
+            }
+
+            if (TypeReport == "PDF")
+            {
+                // Генерация товарной-накладной в формате PDF
+            }
+        }
+    }
+
+    // вариация корректной реализации
+
+    public class IStorageReport
+    {
+        /// <summary>
+        /// Метод для создания товарной-накладной
+        /// </summary>
+        public virtual void GenerateConsignmentNote(Storage storage)
+        {
+            // Базовая реализация, которую нельзя модифицировать
+        }
+    }
+
+    public class StorageCSVReport : IStorageReport
+    {
+        public override void GenerateConsignmentNote(Storage storage)
+        {
+            // Генерация отчета в формате CSV
+        }
+    }
+
+    public class StoragePDFReport : IStorageReport
+    {
+        public override void GenerateConsignmentNote(Storage storage)
+        {
+            // Генерация отчета в формате PDF
+        }
+    }
+
+}
