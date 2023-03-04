ac12a5a5c9f24c1ea7c184da140a1af641b621ed
MagicEaster <101589700+MagicEaster@users.noreply.github.com>
2023-03-04 09:48:19 +0300

    сделан пример с SRP

--- SOLID/Program.cs
+++ SOLID/Program.cs
@@ -8,6 +8,7 @@ namespace SOLID
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
         }
---
+++ SOLID/SRP.cs
@@ -0,0 +1,59 @@
+﻿namespace SOLID
+{
+    //плохой пример
+    public class Storage
+    {
+        public int ID { get; set; }
+        public string GoodsName { get; set; }
+        public int Quantity { get; set; }
+        /// <summary>
+        /// Данный метод добавляет в БД новый товар
+        /// </summary>
+        /// <param name="storage">Объект для вставки</param>
+        /// <returns>Результат вставки новых данных</returns>
+        public bool Add(Storage storage)
+        {
+            // Вставить данные товара в таблицу БД
+            return true;
+        }
+
+        /// <summary>
+        /// товарная-накладная
+        /// </summary>
+        public void GenerateConsignmentNote(Storage storage)
+        {
+            // Генерация товарной-накладной
+        }
+    }
+
+    //правильный вариант
+    public class StorageNew
+    {
+        public int ID { get; set; }
+        public string GoodsName { get; set; }
+        public int Quantity { get; set; }
+        /// <summary>
+        /// Данный метод добавляет в БД новый товар
+        /// </summary>
+        /// <param name="storage">Объект для вставки</param>
+        /// <returns>Результат вставки новых данных</returns>
+        public bool Add(StorageNew storage)
+        {
+            // Вставить данные товара в таблицу БД
+            return true;
+        }
+    }
+
+    public class StorageReport
+    {
+        /// <summary>
+        /// товарная-накладная
+        /// </summary>
+        public void GenerateConsignmentNote(Storage storage)
+        {
+            // Генерация товарной-накладной
+        }
+    }
+}
+    
+    
