fe60c57c25e444e63d346db5aa5e4ea77d8f079b
MagicEaster <101589700+MagicEaster@users.noreply.github.com>
2023-03-04 11:38:54 +0300

    ISP done

---
+++ SOLID/ISP.cs
@@ -0,0 +1,23 @@
+﻿namespace SOLID
+{
+    //в этом интерфейсе несколько ответственностей
+
+    public interface IGoodsISP
+    {
+        bool AddGoodsDetails();
+        bool ShowGoodsDetails(int orderId);
+    }
+
+     // разбиение на два интерфейса
+
+    public interface IOperationAdd
+    {
+        bool AddGoodsDetails();
+    }
+
+    public interface IOperationGet
+    {
+        bool ShowGoodsDetails(int orderId);
+    }
+
+}
